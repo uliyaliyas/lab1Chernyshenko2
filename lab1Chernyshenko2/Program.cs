8f584ddfbb9a6366af3d097ecc7f8f48fe23972c
5090969 <5090969@LAPTOP-NB62C61V>
2023-09-27 20:34:09 +0200

    Добавьте файлы проекта.

---
+++ lab1Chernyshenko2/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите a:");
+double a = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите b:");
+double b = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите h:");
+double h = double.Parse(Console.ReadLine());
+double s1 = a * a;
+double s2 = b * b;
+double v = h * (s1 + Math.Sqrt(s1 * s2) + s2) / 3; Console.WriteLine("Объем усеченной пирамиды:" + v);
